09120d5ee09310d52216c54b212b19876594abce
MohamedElMustafa123 <144330436+MohamedElMustafa123@users.noreply.github.com>
2025-01-15 16:33:42 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 //enable CORS for cross origin request
 app.use(cors(
     {
-        origin: ["https://houndwebdev2.vercel.app/"],
+        origin: ["https://houndwebdev2.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
